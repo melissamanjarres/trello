2d71471a11f69830853e4e1c689aaa46dc615f0f
melissamanjarres <melissaam@uninorte.edu.co>
2015-11-10 03:40:18 +0000

    Really last commit, fixing a mistake

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,5 @@ class User < ActiveRecord::Base
   validates :username, uniqueness: true, presence: true
   validates :first_name, presence: true
   validates :last_name, presence:true
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+ 
 end
